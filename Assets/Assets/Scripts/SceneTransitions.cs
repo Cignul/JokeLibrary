c38d546ef14a3f1b7b04aac9e9d2569ef3bcea2a
Jackson Gordon <40629053+Cignul@users.noreply.github.com>
2018-11-06 07:41:01 -0700

    Add files via upload

---
+++ Assets/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Trying to set up the scene transition
+
+public class SceneTransitions : MonoBehaviour
+{
+    public void SceneTransfer(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
